5f00185a7dce4605c6f2898a6424929a4e91f7bc
Mesbah Uddin <msbh214@gmail.com>
2022-11-25 11:38:09 +0600

    added user sub template for posts

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 from datetime import datetime
 
 from app import db
@@ -21,6 +22,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return "https://www.gravatar.com///avatar/{}?d=identicon&s={}".format(digest, size)
 
 
 class Post(db.Model):
