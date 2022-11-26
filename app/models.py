d0027aff12d8a7cc981abd8d807d2066384ba89c
Mesbah Uddin <msbh214@gmail.com>
2022-11-26 16:32:19 +0600

    added edit profile functionality

--- app/models.py
+++ app/models.py
@@ -12,10 +12,12 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship("Post", backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return "<User {}>".format(self.username)
+        return "<User: username: {} email: {}>".format(self.username, self.email)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
