d93698fa5f8b9d52882e1355e931797f699cb5b5
Mesbah Uddin <msbh214@gmail.com>
2022-12-01 10:45:02 +0600

    added reset password option

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -14,9 +16,11 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
 login.login_view = "login"
 
 from app import routes, models, errors
+
 
 if not app.debug:
     if app.config["MAIL_SERVER"]:
