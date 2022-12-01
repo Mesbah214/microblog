b35f89ea6e6462f22ea637423afcde892fd6ee1b
Mesbah Uddin <msbh214@gmail.com>
2022-12-01 15:29:21 +0600

    added dates and times

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 
@@ -15,6 +16,7 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 mail = Mail(app)
 login.login_view = "login"
